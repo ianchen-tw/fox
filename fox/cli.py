7fde2bd71cb1c51a3e2a477c5c7b2947af349577
Ian Chen <ianre657@gmail.com>
2021-07-11 22:33:44 +0800

    ✨ basic cli interface

--- fox/cli.py
+++ fox/cli.py
@@ -1,8 +1,15 @@
 import argparse
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Optional
 
-import rich
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from pydantic.error_wrappers import ValidationError
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+
+from fox.config import Config
+from fox.dep_manager import DepManager
+from fox.schemas import Semester
 
 if TYPE_CHECKING:
     from dataclasses import dataclass
@@ -15,24 +22,70 @@ else:
 @dataclass
 class TypedArgs(BaseModel):
     year: int
-    term: Literal["1", "2", "x"]
+    term: str
+    gen_dep_cache: bool
+
+    @validator("term")
+    def term_must_be_in_range(cls, val):
+        accepted_values = ["1", "2", "X"]
+        if val not in accepted_values:
+            msg = Text(
+                f"Must be one of [yellow]{accepted_values}[/yellow], get: [red]{val}[/red]"
+            )
+            raise ValueError(msg)
+        return str(val)
 
     @classmethod
-    def from_arg_parse(self, args: argparse.Namespace):
-        pass
+    def from_parsed_args(cls, args) -> Optional["TypedArgs"]:
+        try:
+            instance = cls(
+                gen_dep_cache=args.gen_dep_cache, year=args.year, term=args.term
+            )
+            return instance
+        except ValidationError as e:
+            explain_validation_error(e)
+        return None
+
+
+def explain_validation_error(e: ValidationError):
+    table = Table(padding=1, pad_edge=True)
+    table.title = "[red]Arguemnt failed"
+    table.add_column("Argument", justify="center", style="bold red")
+    table.add_column("Description")
+    for err in e.errors():
+        table.add_row(err["loc"][0], err["msg"])
+    console = Console(
+        force_terminal=True,
+    )
+    console.print(table)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="fox: your tool", epilog="Enjoy!")
-    parser.add_argument("--year", help="academic year")
-    parser.add_argument("--term", help="term")
-    args = parser.parse_args()
-    rich.print(args)
+    parser = argparse.ArgumentParser(prog="fox")
+    parser.add_argument("year", help="academic year")
+    parser.add_argument("term", help="term: [1|2|X]")
+    parser.add_argument(
+        "--gen-dep-cache",
+        action="store_true",
+        help="generate department cache (only for lib development)",
+    )
 
-    # li = Literal["1", "2", "x"]
-    # print(f"literal: {li}")
-    # args = TypedArgs()
-    # rich.print(li)
+    args = TypedArgs.from_parsed_args(parser.parse_args())
+    if args is None:
+        parser.print_help()
+        return -1
+
+    config = Config()
+    if args.gen_dep_cache is True:
+        config.save_dep_result = True
+
+    sem = Semester(year=args.year, term=args.term)
+
+    dep_manager = DepManager(sem)
+    dep_manager.run()
+    dep_manager.get_deps()
+    if config.save_dep_result is True:
+        dep_manager.condense()
 
 
 if __name__ == "__main__":
